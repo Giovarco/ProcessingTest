97384b5cf949a023cde79727d84b202dc66540d5
Unknown <kirby94@hotmail.it>
2019-06-14 13:54:51 +0200

    Draw a creature and food

--- src/evolv/io/EvolvioColor.java
+++ src/evolv/io/EvolvioColor.java
@@ -1,22 +1,36 @@
 package evolv.io;
 
+import evolv.io.Model.Circle;
+import evolv.io.View.CircleView;
 import processing.core.PApplet;
+import processing.core.PVector;
+
+import java.awt.*;
 
 public class EvolvioColor extends PApplet {
 
+    CircleView circleView;
+
+    Circle creature;
+    Circle food;
+
     @Override
     public void settings() {
-
+        size(1000,700);
     }
 
     @Override
     public void setup() {
+        circleView = new CircleView(this.g);
 
+        creature = new Circle(new PVector(500, 500), 10, new Color(255,0,0));
+        food = new Circle(new PVector(700, 600), 10, new Color(0,0,255));
     }
 
     @Override
     public void draw() {
-
+        circleView.draw(creature);
+        circleView.draw(food);
     }
 
 }
